c3a6dba359b020cf167156669b63851f7aaa071e
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-05-02 10:54:40 -0500

    f

--- final.m
+++ final.m
@@ -1,3 +1,3 @@
 encr=[3 2 1;9 2 1;4 5 6]
-encripta('/Users/Ruben/Desktop/4to/Algebra/ProyectoFinal/archivo.txt',encr)
-decripta('/Users/Ruben/Desktop/4to/Algebra/ProyectoFinal/archivo.mau')
+encripta('PATH-TO-FILE',encr)
+decripta('PATH-TO-ENCR-FILE')
