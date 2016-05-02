195f07e4d4950c66dcd4b3556878362bb92417e8
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-05-02 10:44:10 -0500

    ready made

--- final.m
+++ final.m
@@ -88,5 +88,5 @@ endfunction
 
 %encr=[3 2 1;9 2 1;4 5 6]
 %encripta('/Users/Ruben/Desktop/4to/Algebra/ProyectoFinal/archivo.txt',encr)
-decripta('/Users/Ruben/Desktop/4to/Algebra/ProyectoFinal/archivo.mau')
+%decripta('/Users/Ruben/Desktop/4to/Algebra/ProyectoFinal/archivo.mau')
 
